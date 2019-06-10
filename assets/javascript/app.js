3d65724ee4f19a0f10033488876de73e56f17197
Alex Steinberg <arsteinberg93@gmail.com>
2019-06-10 18:04:36 -0400

    html complete, and questions created

---
+++ assets/javascript/app.js
@@ -0,0 +1,26 @@
+var questions = [{
+    q: "How many Stanley Cups have the Red Wings won?",
+    a: ["6", "8", "9", "11"],
+    name: "Redwings",
+    correct: "11",
+}, {
+    q: "How many NBA Championships have the Pistions won?",
+    a: ["2", "3", "4", "5"],
+    name: "Pistons",
+    correct: "3",
+}, {
+    q: "When is the last time the Lions won a championship?",
+    a: ["1957", "1972", "1988", "1999"],
+    name: "Lions",
+    correct: "1957",
+}, {
+    q: "Who is the longest serving captain of the Red Wings?",
+    a: ["Steve Yzerman", "Nick Lidstrom", "Gordie Howe", "Ted Lindsay"],
+    name: "Stevie",
+    correct: "Steve Yzerman",
+}, {
+    q: "What is the Detroit baseball teams mascot?",
+    a: ["Bears", "Tigers", "Barracudas", "Falcons"],
+    name: "Tigers",
+    correct: "Tigers",
+}];
